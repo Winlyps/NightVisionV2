4d35fe62372af4597f267736f376d11a611dffd2
Winlypse <nts3@o2.pl>
2025-08-07 23:13:33 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/nightVision/NightVision.kt
@@ -0,0 +1,14 @@
+package winlyps.nightVision
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NightVision : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
